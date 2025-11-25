d23fb5b4697cdb8247e870ba837541b210313fe9
Emilly-Freire <emillyfreireluna17@gmail.com>
2025-11-25 07:45:29 -0300

    adicionando typed

---
+++ docs/js/script.js
@@ -0,0 +1,14 @@
+
+const typed = new Typed(".multiple-text", {
+    strings: [
+        "estudante",
+        "dev em aprendizado",
+        "flautista",
+        "desenhista"
+    ],
+    
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
